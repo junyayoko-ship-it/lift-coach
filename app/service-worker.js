32c06fa6f0f1844c9f264d8d487a653a5d343f89
junyayoko-ship-it <junya.yokoyama0121@gmail.com>
2026-01-11 14:13:13 +0700

    Create service-worker.js

---
+++ app/service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "liftcoach-v1";
+const APP_SHELL = [
+  "/app/index.html",
+  "/app/styles.css",
+  "/app/app.js",
+  "/app/manifest.json",
+  "/app/icons/icon-192.png",
+  "/app/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).then((res) => {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          return res;
+        })
+      );
+    })
+  );
+});
