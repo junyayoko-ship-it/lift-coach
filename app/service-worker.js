b9b24b553dbd16f1c6846a1c952148ad5a454554
junyayoko-ship-it <junya.yokoyama0121@gmail.com>
2026-08-07 15:25:07 +0700

    Refresh app cache for sync release

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "liftcoach-v2";
+const CACHE_NAME = "liftcoach-v3";
 const APP_SHELL = ["./index.html", "./styles.css", "./app.js", "./manifest.json"];
 self.addEventListener("install", (event) => { event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))); self.skipWaiting(); });
 self.addEventListener("activate", (event) => { event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))); self.clients.claim(); });
